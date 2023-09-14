35249529794193e97d0aa7b22ebede60a3cea8bd
yurikaza <yusufmirza55@hotmail.com>
2023-09-14 12:27:22 +0300

    a

--- core/node/node.go
+++ core/node/node.go
@@ -39,10 +39,11 @@ func NewNode(ctx context.Context) (p2phost.Host, error) {
 		return nil, err
 	}
 	log.SetLogLevel("synnefo", "DEBUG")
+
 	// Create a list of libp2p options, including the DHT option
 	opts := []libp2p.Option{
-		//libp2p.DisableRelay(),     // Disable relay (optional)
-		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/4001"),
+		libp2p.DisableRelay(), // Disable relay (optional)
+		//libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/4001"),
 		//libp2p.Transport(tcp.NewTCPTransport),
 		libp2p.EnableRelay(),
 		libp2p.EnableNATService(), // Enable NAT service (optional)
@@ -51,7 +52,7 @@ func NewNode(ctx context.Context) (p2phost.Host, error) {
 		libp2p.Muxer("/yamux/1.0.0", yamux.DefaultTransport),
 		libp2p.Identity(key),
 		libp2p.Ping(true),
-		libp2p.Security("synnefo", ctx),
+		//libp2p.Security("/libp2p/autonat/1.0.0", ctx),
 		/*
 			libp2p.Routing(func(h p2phost.Host) (routing.PeerRouting, error) {
 				dht, err := dht.New(ctx, h)
