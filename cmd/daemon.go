a2b7282717eb345930cd873f5538d256cedfc6c1
yurikaza <yusufmirza55@hotmail.com>
2023-09-17 15:01:43 +0300

    a

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -177,9 +177,8 @@ to quickly create a Cobra application.`,
 		fmt.Println(listPeers)
 
 		host.Host.SetStreamHandler(node, "/synnefo/1.0.0", func(s network.Stream) {
+			go writeCounter(s)
 			go readCounter(s)
-
-			s.Close()
 		})
 
 		hostInfor := host.InfoFromHost(node)
@@ -215,9 +214,8 @@ to quickly create a Cobra application.`,
 				panic(err)
 			}
 
+			go writeCounter(s)
 			go readCounter(s)
-
-			defer s.Close()
 
 		}
 
